925c3998c60f172b8def5548e7aa75bd5ba4dc4f
shani <shaniromanov@gmail.com>
2020-02-19 00:05:22 +0200

    fized the ssl

--- N_puzzle/Node.cs
+++ N_puzzle/Node.cs
@@ -11,6 +11,7 @@ namespace N_puzzle
         public State State { get; }
         public Node Parent { get; }
         public Direction Direction { get; }
-        ///
+        //
+        //
     }
 }
