6f23021846e4f6856f145af3b31fd1e3a8cfb9d0
shani <shaniromanov@gmail.com>
2020-02-18 23:16:41 +0200

    start the nodes, and the base classes

---
+++ N_puzzle/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_puzzle
+{
+    class Node
+    {
+        public State State { get; }
+        public Node Parent { get; }
+        public Direction Direction { get; }
+    }
+}
